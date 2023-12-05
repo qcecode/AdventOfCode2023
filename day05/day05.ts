37379c8fc679f39ba7694f966476a4a024ddbfec
Henri S <henri.schmeing@gmail.com>
2023-12-05 12:56:50 +0100

    day05 part 2

--- day05/day05.ts
+++ day05/day05.ts
@@ -1,22 +1,25 @@
 import fs from 'fs';
 
-type MapEntry = number[][];
-type ParsedMaps = { [key: string]: MapEntry };
+function parseInputFile(filePath: string): { seeds: number[], maps: { [key: string]: number[][] } } {
 
-function parseInputFile(filePath: string): { seeds: number[], maps: ParsedMaps } {
     const fileContent = fs.readFileSync(filePath, 'utf8');
+
     const sections = fileContent.split('\n\n');
 
-    const seedsLine = sections.shift() || "";
-    const seeds = seedsLine.split(':')[1].trim().split(' ').map(Number);
+    const maps: { [key: string]: number[][] } = {};
 
-    const maps = sections.reduce((acc: ParsedMaps, section) => {
-        const [title, ...dataLines] = section.split('\n');
-        const key = title.split(':')[0].trim();
-        acc[key] = dataLines.map(line => line.split(' ').map(Number));
-        return acc;
-    }, {});
+    const seedsLine = sections.shift() || ""; // Entfernt und speichert die erste Zeile
+    const seeds = seedsLine.substring(seedsLine.indexOf(':') + 1).trim().split(' ').map(Number);
 
+    sections.forEach(section => {
+
+        const lines = section.split('\n');
+        const key = lines[0].split(':')[0].trim();
+        const dataLines = lines.slice(1);
+
+        maps[key] = dataLines.map(line => line.split(' ').map(Number));
+
+    });
     return {seeds, maps};
 }
 
@@ -41,32 +44,76 @@ class MapsData {
 
 }
 
-function processSeed(seed: number, map: MapEntry): number {
-    return map.reduce((processedSeed, [output, min, range]) => {
-        if (seed >= min && seed <= min + range) {
-            return output + (seed - min);
+function processSeed(seed: number, map: number[][]): number {
+    for (const mapElement of map) {
+        if (seed >= mapElement[1] && seed <= mapElement[1] + mapElement[2]) {
+            return mapElement[0] + (seed - mapElement[1]);
         }
-        return processedSeed;
-    }, seed);
+    }
+    return seed;
 }
 
-function findMinimumProcessedSeed(seeds: number[], maps: MapsData): number {
-    return seeds.reduce((minSeed, seed) => {
-        const processedSeed = processAllMaps(seed, maps);
-        return Math.min(minSeed, processedSeed);
-    }, Number.MAX_VALUE);
+function processSeeds(seeds: number[], maps: MapsData): number {
+    let minSeed = Number.MAX_VALUE;
+    for (const seed of seeds) {
+        let inter = processSeed(seed, maps.seedToSoil);
+        inter = processSeed(inter, maps.soilToFertilizer);
+        inter = processSeed(inter, maps.fertilizerToWater);
+        inter = processSeed(inter, maps.waterToLight);
+        inter = processSeed(inter, maps.lightToTemperature);
+        inter = processSeed(inter, maps.temperatureToHumidity);
+        inter = processSeed(inter, maps.humidityToLocation);
+        if (minSeed >= inter) {
+            minSeed = inter;
+        }
+    }
+    return minSeed;
 }
 
-function processAllMaps(seed: number, maps: MapsData): number {
-    return [maps.seedToSoil, maps.soilToFertilizer, maps.fertilizerToWater,
-        maps.waterToLight, maps.lightToTemperature, maps.temperatureToHumidity,
-        maps.humidityToLocation]
-        .reduce((intermediateSeed, map) => processSeed(intermediateSeed, map), seed);
+function precessMoreSeeds(seeds: number[], maps: MapsData): number {
+    const maxSeed = Math.max(...seeds);
+    for (let i = 0; i < maxSeed; i++) {
+        let inter = reverseProcessSeed(i, maps.humidityToLocation);
+        inter = reverseProcessSeed(inter, maps.temperatureToHumidity);
+        inter = reverseProcessSeed(inter, maps.lightToTemperature);
+        inter = reverseProcessSeed(inter, maps.waterToLight);
+        inter = reverseProcessSeed(inter, maps.fertilizerToWater);
+        inter = reverseProcessSeed(inter, maps.soilToFertilizer);
+        inter = reverseProcessSeed(inter, maps.seedToSoil);
+
+        if (isInRange(inter, seeds)) {
+            return i;
+        }
+    }
+    return maxSeed;
+}
+
+function isInRange(num: number, seeds: number[]) {
+    for (let i = 0; i < seeds.length; i = i + 2) {
+        if (num >= seeds[i] && num <= seeds[i + 1] + seeds[i]) {
+            return true;
+        }
+    }
+    return false
+}
+
+function reverseProcessSeed(seed: number, map: number[][]): number {
+    for (const mapElement of map) {
+        if (seed >= mapElement[0] && seed < mapElement[0] + mapElement[2]) {
+            return mapElement[1] + (seed - mapElement[0]);
+        }
+    }
+    return seed;
 }
 
 const parsedFile = parseInputFile('input.txt');
 const mapsData = new MapsData(parsedFile.maps);
-const minProcessedSeed = findMinimumProcessedSeed(parsedFile.seeds, mapsData);
-console.log(minProcessedSeed);
 
+// Part1
+const minProcessedSeed = processSeeds(parsedFile.seeds, mapsData);
+console.log("Part 1: " + minProcessedSeed);
+
+// Part2
+const minProcessedSeedPartTwo = precessMoreSeeds(parsedFile.seeds, mapsData);
+console.log("Part 2: " + minProcessedSeedPartTwo);
 
